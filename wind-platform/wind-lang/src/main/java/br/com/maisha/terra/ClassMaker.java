c2cb4d6a8355ed69a68b5e189f4a5b08e654cb37
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2009-06-17 01:48:12 +0000

    changing the parser to return the domain object itself and no more a map of domain objects once only one object is parsed at a time.

---
+++ wind-platform/wind-lang/src/main/java/br/com/maisha/terra/ClassMaker.java
@@ -0,0 +1,88 @@
+package br.com.maisha.terra;
+
+import org.apache.commons.lang.StringUtils;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtField;
+import javassist.CtMethod;
+import br.com.maisha.terra.lang.Attribute;
+import br.com.maisha.terra.lang.DomainObject;
+import br.com.maisha.wind.common.exception.MakeClassException;
+
+/**
+ * 
+ * @author Paulo Freitas (pfreitas1@gmail.com)
+ * 
+ */
+public class ClassMaker implements IClassMaker {
+
+	/**
+	 * 
+	 * @see br.com.maisha.terra.IClassMaker#make(br.com.maisha.terra.lang.DomainObject)
+	 */
+	public Class<?> make(DomainObject obj) throws MakeClassException {
+		try {
+			ClassPool pool = ClassPool.getDefault();
+			CtClass cc = pool.makeClass(obj.getRef());
+
+			String field;
+			for (Attribute att : obj.getAtts()) {
+				field = "private " + att.getType() + " " + att.getRef() + ";";
+				cc.addField(CtField.make(field, cc));
+				cc.addMethod(CtMethod.make(genSetMethod(att), cc));
+				cc.addMethod(CtMethod.make(genGetMethod(att), cc));
+			}
+			
+
+			return cc.toClass();
+		} catch (CannotCompileException e) {
+			throw new MakeClassException(e);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param att
+	 * @return
+	 */
+	private String genSetMethod(Attribute att){
+		StringBuilder method = new StringBuilder();
+		
+		method.append("public void set");
+		method.append(StringUtils.capitalize(att.getRef()));
+		method.append("(");
+		method.append(att.getType());
+		method.append(" v ){");
+		method.append("this.");
+		method.append(att.getRef());
+		method.append(" = v;");
+		method.append("}");
+		
+		return method.toString();
+	}
+	
+	
+	/**
+	 * 
+	 * @param att
+	 * @return
+	 */
+	private String genGetMethod(Attribute att){
+		StringBuilder method = new StringBuilder();
+		
+		method.append("public ");
+		method.append(att.getType());
+		method.append(" get");
+		method.append(StringUtils.capitalize(att.getRef()));
+		method.append("(){");
+		method.append("return this.");
+		method.append(att.getRef());
+		method.append(";");
+		method.append("}");
+		
+		return method.toString();
+	}
+
+}
