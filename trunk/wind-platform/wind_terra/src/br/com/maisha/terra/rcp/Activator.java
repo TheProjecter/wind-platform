888ea68ad491625dcfd5e22f576dd0e60e0420fd
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2009-08-11 23:47:30 +0000

    add log dependency

--- trunk/wind-platform/wind_terra/src/br/com/maisha/terra/rcp/Activator.java
+++ trunk/wind-platform/wind_terra/src/br/com/maisha/terra/rcp/Activator.java
@@ -1,24 +1,32 @@
 package br.com.maisha.terra.rcp;
 
+import org.apache.log4j.Logger;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
+	private static Logger log = Logger.getLogger(Activator.class);
+
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
-		System.out.println("Hello World!!");
+		log.debug("		Wind Terra Started");
 	}
-	
+
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		System.out.println("Goodbye World!!");
+		System.out.println(" 	Wind Terra Stopped");
 	}
 
 }
