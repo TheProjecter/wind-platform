7ef26870c490566ef7fe1dd7979cc3709c5c49b9
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2009-08-11 23:33:42 +0000

    update to use new ServiceProvider

--- trunk/wind-platform/wind-app-registry/src/br/com/maisha/wind/lifecycle/WindActivator.java
+++ trunk/wind-platform/wind-app-registry/src/br/com/maisha/wind/lifecycle/WindActivator.java
@@ -2,8 +2,10 @@ package br.com.maisha.wind.lifecycle;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkEvent;
+import org.osgi.framework.FrameworkListener;
 
-import br.com.maisha.wind.common.factory.SpringBeanFactory;
+import br.com.maisha.wind.common.factory.ServiceProvider;
 import br.com.maisha.wind.lifecycle.mgmt.IApplicationManager;
 
 /**
@@ -23,9 +25,23 @@ public class WindActivator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 
-		IApplicationManager appManager = SpringBeanFactory.getInstance()
-				.getService(IApplicationManager.class);
-		appManager.registerApplication(context);
+		// IApplicationManager appManager = SpringBeanFactory.getInstance()
+		// .getService(IApplicationManager.class);
+
+		IApplicationManager appManager = ServiceProvider.getInstance()
+				.getService(IApplicationManager.class,
+						getBundle().getBundleContext());
+		appManager.registerApplication(getBundle().getBundleContext());
+
+		context.addFrameworkListener(new FrameworkListener() {
+			@Override
+			public void frameworkEvent(FrameworkEvent event) {
+				if (event.getType() == FrameworkEvent.STARTED) {
+					System.out.println("### FrameworkEvent.STARTED");
+
+				}
+			}
+		});
 	}
 
 	/**
