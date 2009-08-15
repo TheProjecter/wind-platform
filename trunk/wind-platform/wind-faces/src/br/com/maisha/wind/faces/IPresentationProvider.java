34f1c0c043a377df3e2f407fd4f85fb7d1358080
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2009-08-15 18:36:15 +0000

    javadoc; BaseRender

--- trunk/wind-platform/wind-faces/src/br/com/maisha/wind/faces/IPresentationProvider.java
+++ trunk/wind-platform/wind-faces/src/br/com/maisha/wind/faces/IPresentationProvider.java
@@ -6,30 +6,54 @@ import br.com.maisha.wind.common.listener.IAppRegistryListener.LevelType;
 import br.com.maisha.wind.faces.render.IRender;
 
 /**
+ * The Presentation Provider is responsible for the conversation between the
+ * model and the user interface. Every change in model causes the rerender of
+ * the user interface.
  * 
+ * The render process consists in calling all of the registered {@link IRender}.
+ * Each render recieves the model and renders it in a proper way.
+ * 
+ * @see FacesAppModelListener
  * @author Paulo Freitas (pfreitas1@gmail.com)
  * 
  */
 public interface IPresentationProvider extends Component {
 
 	/**
+	 * Renders de given model, based on the level and type of the change. See
+	 * {@link LevelType} and {@link ChangeType} for possible values.
 	 * 
+	 * @see LevelType
+	 * @see ChangeType
 	 * @param model
+	 *            Model object which has changed.
 	 * @param level
+	 *            The level model is at.
 	 * @param change
+	 *            Type of the change.
 	 */
 	void render(Object model, LevelType level, ChangeType change);
 
 	/**
+	 * Registers the given render.
 	 * 
 	 * @param render
 	 */
 	void registerRender(IRender render);
 
 	/**
+	 * Remove the given render.
 	 * 
 	 * @param render
 	 */
 	void removeRender(IRender render);
+
+	/**
+	 * Process the click of the menu identified by the given Id.
+	 * 
+	 * @param menuId
+	 *            Id of the menu that was clicked.
+	 */
+	void processMenu(String menuId);
 
 }
