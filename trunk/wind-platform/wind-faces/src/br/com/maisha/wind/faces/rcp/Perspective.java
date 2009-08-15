baa142a872cbe1d90c7c47442e36d22b6ef268b8
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2009-08-15 19:45:53 +0000

    adjusts to the perspective; EditionView

--- trunk/wind-platform/wind-faces/src/br/com/maisha/wind/faces/rcp/Perspective.java
+++ trunk/wind-platform/wind-faces/src/br/com/maisha/wind/faces/rcp/Perspective.java
@@ -3,23 +3,31 @@ package br.com.maisha.wind.faces.rcp;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+import br.com.maisha.wind.faces.view.EditionView;
 import br.com.maisha.wind.faces.view.ModuleOverview;
-import br.com.maisha.wind.faces.view.View;
 
 /**
  * Configures the perspective layout. This class is contributed through the
  * plugin.xml.
+ * 
+ * @author Paulo Freitas (pfreitas1@gmail.com)
+ * 
  */
 public class Perspective implements IPerspectiveFactory {
 
+	/**
+	 * 
+	 * @see org.eclipse.ui.IPerspectiveFactory#createInitialLayout(org.eclipse.ui.IPageLayout)
+	 */
 	public void createInitialLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
 		layout.setEditorAreaVisible(false);
 		layout.setFixed(true);
 
-		layout.addStandaloneView(View.ID, false, IPageLayout.LEFT, .5f,
+		layout.addStandaloneView(ModuleOverview.ID, false, IPageLayout.LEFT, .3f,
 				editorArea);
 
-		layout.addView(ModuleOverview.ID, IPageLayout.RIGHT, .5f, editorArea);
+		layout.addView(EditionView.ID, IPageLayout.RIGHT, .7f,
+				editorArea);
 	}
 }
