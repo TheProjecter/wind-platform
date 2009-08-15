cd9216ce20ee310a648e2c83634b112a384b8647
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2009-08-15 19:45:53 +0000

    adjusts to the perspective; EditionView

---
+++ wind-platform/wind-faces/src/br/com/maisha/wind/faces/view/EditionView.java
@@ -0,0 +1,36 @@
+package br.com.maisha.wind.faces.view;
+
+import org.apache.log4j.Logger;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * 
+ * @author Paulo Freitas (pfreitas1@gmail.com)
+ * 
+ */
+public class EditionView extends ViewPart {
+
+	/** View's ID. */
+	public static final String ID = "wind_faces.edition_view";
+
+	/** Log ref. */
+	private static final Logger log = Logger.getLogger(EditionView.class);
+	
+	/**
+	 * 
+	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+	 */
+	public void createPartControl(Composite parent) {
+
+	}
+
+	/**
+	 * 
+	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
+	 */
+	public void setFocus() {
+
+	}
+
+}
