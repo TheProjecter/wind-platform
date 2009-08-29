2482f1ec0f049a60298ca55e041d53ed8fdc229d
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2009-08-29 17:18:20 +0000

    commons dependency

--- trunk/wind-platform/wind_terra/src/br/com/maisha/terra/rcp/Activator.java
+++ trunk/wind-platform/wind_terra/src/br/com/maisha/terra/rcp/Activator.java
@@ -1,12 +1,15 @@
 package br.com.maisha.terra.rcp;
 
 import org.apache.log4j.Logger;
-import org.osgi.framework.BundleActivator;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
-public class Activator implements BundleActivator {
+public class Activator extends AbstractUIPlugin {
 
 	private static Logger log = Logger.getLogger(Activator.class);
+
+	// The shared instance
+	private static Activator plugin;
 
 	/*
 	 * (non-Javadoc)
@@ -17,6 +20,8 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext context) throws Exception {
 		log.info("		Wind Terra Starting... ");
+		super.start(context);
+		plugin = this;
 		log.info("		Wind Terra Started");
 	}
 
@@ -27,7 +32,18 @@ public class Activator implements BundleActivator {
 	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
 		log.info("		Wind Terra Stopped");
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
 	}
 
 }
