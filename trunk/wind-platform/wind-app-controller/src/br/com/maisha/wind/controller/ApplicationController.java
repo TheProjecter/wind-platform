6a660dc3f5650b77f5cbbecd7121470c95a3fae2
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2010-04-28 19:31:03 +0000

    evolution

--- trunk/wind-platform/wind-app-controller/src/br/com/maisha/wind/controller/ApplicationController.java
+++ trunk/wind-platform/wind-app-controller/src/br/com/maisha/wind/controller/ApplicationController.java
@@ -10,6 +10,7 @@ import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
+import org.apache.log4j.Logger;
 import org.osgi.framework.BundleContext;
 
 import br.com.maisha.terra.lang.Attribute;
@@ -18,38 +19,48 @@ import br.com.maisha.terra.lang.ModelReference;
 import br.com.maisha.terra.lang.Operation;
 import br.com.maisha.terra.lang.Property;
 import br.com.maisha.terra.lang.PropertyInfo;
+import br.com.maisha.wind.common.Activator;
+import br.com.maisha.wind.common.converter.IConverterService;
+import br.com.maisha.wind.common.factory.ServiceProvider;
 import br.com.maisha.wind.controller.execution.api.RuleAPI;
 import br.com.maisha.wind.controller.model.ExecutionContext;
 
 /**
- * TODO javadoc.
+ * 
  * 
  * @author Paulo Freitas (pfreitas1@gmail.com)
  * 
  */
 public class ApplicationController implements IApplicationController {
 
+	/** Log ref. */
+	private static final Logger log = Logger
+			.getLogger(ApplicationController.class);
+
+	/** Reference to the script engine manager. */
 	private ScriptEngineManager engineManager = new ScriptEngineManager();
 
 	/**
 	 * 
 	 * @see br.com.maisha.wind.controller.IApplicationController#runOperation(br.com.maisha.wind.controller.model.ExecutionContext)
 	 */
-	public ExecutionContext<ModelReference> runOperation(ExecutionContext<ModelReference> ctx) {
+	@SuppressWarnings("unchecked")
+	public ExecutionContext<ModelReference> runOperation(
+			ExecutionContext<ModelReference> ctx) {
 
 		try {
 			// validation phase
-			
-			
-			
+
 			// run operation
 			Operation op = ctx.getOperation();
 			ScriptEngine engine = engineManager.getEngineByName(op.getType());
 			Invocable invocable = (Invocable) engine;
 
-			BundleContext bundle = ctx.getOperation().getDomainObject().getApplication().getBundleContext();
+			BundleContext bundle = ctx.getOperation().getDomainObject()
+					.getApplication().getBundleContext();
 
-			URL ruleUrl = bundle.getBundle().getEntry("/src/" + op.getPropertyValue(PropertyInfo.FILE));
+			URL ruleUrl = bundle.getBundle().getEntry(
+					"/src/" + op.getPropertyValue(PropertyInfo.FILE));
 
 			InputStream is = ruleUrl.openStream();
 			Reader r = new InputStreamReader(is);
@@ -60,7 +71,8 @@ public class ApplicationController implements IApplicationController {
 			engine.eval("rule = " + op.getRef() + "(ctx, api)");
 			Object o = engine.get("rule");
 
-			ctx = (ExecutionContext<ModelReference>) invocable.invokeMethod(o, "execute");
+			ctx = (ExecutionContext<ModelReference>) invocable.invokeMethod(o,
+					"execute");
 
 		} catch (Exception e) {
 			e.printStackTrace(); // TODO handle
@@ -74,19 +86,29 @@ public class ApplicationController implements IApplicationController {
 	 */
 	public void evalExpressions(ModelReference modelInstance) {
 		try {
+			IConverterService convService = ServiceProvider
+					.getInstance()
+					.getService(IConverterService.class, Activator.getDefault());
 			ScriptEngine juelEngine = engineManager.getEngineByName("juel");
-
 			DomainObject meta = modelInstance.getMeta();
 
+			log.debug("Evaluating expressions for " + meta.getLabel());
+
 			juelEngine.put("this", modelInstance);
+			juelEngine.put("meta", meta);
 
 			for (Attribute attr : meta.getAtts()) {
-				for (Map.Entry<String, Property> entry : attr.getProperties().entrySet()) {
+				for (Map.Entry<String, Property> entry : attr.getProperties()
+						.entrySet()) {
 					Property prop = entry.getValue();
 
 					if (prop.getExpression() != null) {
 						Object ret = juelEngine.eval(prop.getExpression());
+						ret = convService.convert(PropertyInfo.getPropertyInfo(
+								prop.getPropName()).getType(), ret);
 						if (ret != null) {
+							log.debug("Property value [" + prop.getPropName()
+									+ "] evaluated to: " + ret);
 							prop.setValue(ret);
 						}
 					}
