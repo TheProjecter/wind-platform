6329c5d62af600cf22e74122cfecec3b6d7cefbd
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2010-04-29 19:56:29 +0000

    message

--- trunk/wind-platform/wind-faces/src/br/com/maisha/wind/faces/render/attr/CheckboxRenderer.java
+++ trunk/wind-platform/wind-faces/src/br/com/maisha/wind/faces/render/attr/CheckboxRenderer.java
@@ -1,14 +1,18 @@
 package br.com.maisha.wind.faces.render.attr;
 
 import org.apache.log4j.Logger;
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.beans.BeansObservables;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 
 import br.com.maisha.terra.lang.Attribute;
 import br.com.maisha.terra.lang.ModelReference;
-import br.com.maisha.terra.lang.PropertyInfo;
 import br.com.maisha.terra.lang.Property.PresentationType;
 
 /**
@@ -38,20 +42,27 @@ public class CheckboxRenderer extends BaseAttrRender {
 	public void render(Attribute attr, Composite parent,
 			ModelReference modelInstance) {
 		log.debug("Starting render for attr [" + attr + "] ");
-		
+
 		Button checkbox = new Button(parent, SWT.CHECK);
 		checkbox.setData(attr.getRef());
 		checkbox.setText(attr.getLabel());
 		checkbox.setToolTipText(attr.getLabel());
-		
+
 		GridData gd = getLayoutData();
 		checkbox.setLayoutData(gd);
 		setDimensions(gd, attr);
 
 		setColspan(gd, attr);
 		setRowspan(gd, attr);
-		
-		checkbox.setEnabled(!attr.getPropertyValue(PropertyInfo.DISABLED));
+
+		// configure common bindings
+		DataBindingContext dbctx = configureDataBindings(checkbox, null, attr);
+
+		// configure value binding
+		IObservableValue observable = BeansObservables.observeValue(
+				modelInstance, attr.getRef());
+		dbctx.bindValue(SWTObservables.observeSelection((Control) checkbox),
+				observable);
 	}
 
 }
