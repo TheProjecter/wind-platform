102f81e97075e80e5385a9b6cb8ce40d13dfe923
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2010-04-29 18:41:22 +0000

    message

---
+++ wind-platform/wind-common/src/br/com/maisha/wind/common/listener/IAppModelListenerRegistry.java
@@ -0,0 +1,36 @@
+package br.com.maisha.wind.common.listener;
+
+import br.com.maisha.wind.common.component.Component;
+import br.com.maisha.wind.common.listener.IAppRegistryListener;
+import br.com.maisha.wind.common.listener.IAppRegistryListener.ChangeType;
+import br.com.maisha.wind.common.listener.IAppRegistryListener.LevelType;
+
+/**
+ * 
+ * @author Paulo Freitas (pfreitas1@gmail.com)
+ * 
+ */
+public interface IAppModelListenerRegistry extends Component {
+
+	/**
+	 * 
+	 * @param listener
+	 */
+	void registerAppModelListener(IAppRegistryListener listener);
+
+	/**
+	 * 
+	 * @param listener
+	 */
+	void removeAppModelListener(IAppRegistryListener listener);
+
+	/**
+	 * 
+	 * @param oldValue
+	 * @param newValue
+	 * @param level
+	 * @param change
+	 */
+	void fireEvent(Object oldValue, Object newValue, LevelType level,
+			ChangeType change);
+}
