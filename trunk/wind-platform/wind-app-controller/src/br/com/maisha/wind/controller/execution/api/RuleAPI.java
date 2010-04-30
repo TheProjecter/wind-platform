ed0c8fb2a08a7f910283b22679f41997c953d62c
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2010-04-30 20:00:29 +0000

    window

--- trunk/wind-platform/wind-app-controller/src/br/com/maisha/wind/controller/execution/api/RuleAPI.java
+++ trunk/wind-platform/wind-app-controller/src/br/com/maisha/wind/controller/execution/api/RuleAPI.java
@@ -1,5 +1,7 @@
 package br.com.maisha.wind.controller.execution.api;
 
+import org.apache.commons.lang.StringUtils;
+
 import br.com.maisha.terra.lang.ModelReference;
 import br.com.maisha.wind.controller.model.ExecutionContext;
 import br.com.maisha.wind.controller.model.UserMessage;
@@ -12,20 +14,73 @@ import br.com.maisha.wind.controller.model.UserMessage.MessageKind;
  */
 public class RuleAPI {
 
+	/** */
 	private ExecutionContext<ModelReference> ctx;
 
+	/**
+	 * 
+	 * @param ctx
+	 */
 	public RuleAPI(ExecutionContext<ModelReference> ctx) {
 		this.ctx = ctx;
 	}
 
+	/**
+	 * 
+	 * @param message
+	 */
 	public void info(String message) {
-		UserMessage um = new UserMessage(MessageKind.INFO, message);
-		ctx.getMessages().add(um);
+		message(MessageKind.INFO, message);
 	}
-	
+
+	/**
+	 * 
+	 * @param message
+	 */
+	public void error(String message) {
+		message(MessageKind.ERROR, message);
+	}
+
+	/**
+	 * 
+	 * @param message
+	 */
+	public void success(String message) {
+		message(MessageKind.SUCCESS, message);
+	}
+
+	/**
+	 * 
+	 * @param message
+	 */
 	public void warn(String message) {
-		UserMessage um = new UserMessage(MessageKind.WARNING, message);
+		message(MessageKind.WARNING, message);
+	}
+
+	/**
+	 * @param kind
+	 * @param message
+	 */
+	private void message(MessageKind kind, String message) {
+		UserMessage um = new UserMessage(kind, message);
 		ctx.getMessages().add(um);
 	}
 
+	/**
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public boolean notNull(Object value) {
+		return value != null;
+	}
+
+	/**
+	 * 
+	 * @param string
+	 * @return
+	 */
+	public boolean notEmpty(String string) {
+		return !StringUtils.isEmpty(string) && !StringUtils.isBlank(string);
+	}
 }
