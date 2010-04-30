53e32a5e40200c6bc664b12437a7acd4181bf1eb
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2010-04-30 17:38:15 +0000

    progress

--- trunk/wind-platform/wind-sample/src/CalculateCapacity.py
+++ trunk/wind-platform/wind-sample/src/CalculateCapacity.py
@@ -1,4 +1,5 @@
 from types import *
+import time
 
 class CalculateCapacity:
 
@@ -15,4 +16,13 @@ class CalculateCapacity:
 		self.model.capacity = int(self.model.weight / 10)
 		api.info("airplane.passengerscapacity.success")
 		#self.model.meta.getAttribute("code").getProperties().get("required").setValue("false")
+		
+		ctx.monitor.worked(10)
+		time.sleep(2)
+		ctx.monitor.worked(10)
+		time.sleep(2)
+		ctx.monitor.worked(10)
+		time.sleep(2)
+		ctx.monitor.worked(10)
+		
 		return self.ctx
