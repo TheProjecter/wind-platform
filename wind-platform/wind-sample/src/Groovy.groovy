d2a4d38e02b2be0cc06759b67871479de8228a2b
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2010-05-04 19:25:44 +0000

    groovy metaprogramming

--- wind-platform/wind-sample/src/Groovy.groovy
+++ wind-platform/wind-sample/src/Groovy.groovy
@@ -7,15 +7,18 @@ class GroovyAction{
 	def model
 
 	GroovyAction(ctx, api){
-		println "init Groov"
 		this.ctx = ctx
 		this.api = api
 		this.model = ctx.instance
 	}
 	
 	Object execute(){
-		println "execute"
+		String.metaClass.warn={->
+			api.warn(delegate)
+		}
+	
 		api.warn("i'm comming")
+		"doxo".warn();
 		model.source = "i'm comming"
 		return ctx
 	}
