49f4ddb52b21088e6e2c16386781c98727500b31
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2010-05-20 16:27:30 +0000

    adding locale support

--- wind-platform/wind-app-registry/src/br/com/maisha/wind/lifecycle/mgmt/AppCfgReader.java
+++ wind-platform/wind-app-registry/src/br/com/maisha/wind/lifecycle/mgmt/AppCfgReader.java
@@ -25,10 +25,12 @@ public class AppCfgReader implements IAppCfgReader {
 		d.addObjectCreate("app", WindApplication.class);
 		d.addSetProperties("app", "name", "name");
 		d.addSetProperties("app", "appId", "appId");
+		d.addSetProperties("app", "locale", "defaultLocale");
 
 		d.addObjectCreate("app/bundle", ResourceBundleEntry.class);
 		d.addSetProperties("app/bundle", "id", "id");
 		d.addSetProperties("app/bundle", "path", "path");
+		d.addSetProperties("app/bundle", "locale", "locale");
 
 		d.addSetNext("app/bundle", "addResourceBundle");
 
