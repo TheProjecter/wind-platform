c28e1f34dc53030f4e5074e27320899f539ceda9
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2010-06-03 17:17:26 +0000

    preferences

---
+++ wind-platform/wind-preferences/src/br/com/maisha/preferences/PreferenceStore.java
@@ -0,0 +1,17 @@
+package br.com.maisha.preferences;
+
+/**
+ * Specifies the names of the nodes stored as preferences on the
+ * 
+ * @author Paulo Freitas (pfreitas1@gmail.com)
+ * 
+ */
+public class PreferenceStore {
+
+	public static final String GENERAL = "general";
+
+	public static final String LOCALE = "currentLocale";
+
+	public static final String MAXRESULTSDISPLAYED = "maxResultsDisplayed";
+
+}
---
+++ wind-platform/wind-preferences/src/br/com/maisha/preferences/rcp/Activator.java
@@ -0,0 +1,49 @@
+package br.com.maisha.preferences.rcp;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import br.com.maisha.wind.lifecycle.WindActivator;
+
+/**
+ * 
+ * @author Paulo Freitas (pfreitas1@gmail.com)
+ * 
+ */
+public class Activator extends WindActivator implements BundleActivator {
+
+	/** */
+	private static Activator plugin;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+}
