7c7aac8cb411b5ca9a6dacd4f1268562d43218cf
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2010-06-04 20:22:14 +0000

    rule evolution

--- wind-platform/wind-sample/src/CalculateCapacity.py
+++ wind-platform/wind-sample/src/CalculateCapacity.py
@@ -9,12 +9,8 @@ class CalculateCapacity:
 		self.api = api
 
 	def execute(self):
-		if type(self.model.weight) is NoneType:
-			api.warn(self.model.meta, "airplane.passengerscapacity.weight.required", ["Peso"])
-			return self.ctx
-			
 		self.model.capacity = int(self.model.weight / 10)
-		api.info(self.model.meta, "airplane.passengerscapacity.success", [])
+		
 		#self.model.meta.getAttribute("code").getProperties().get("required").setValue("false")
 		
 		ctx.monitor.worked(10)
@@ -28,4 +24,5 @@ class CalculateCapacity:
 		time.sleep(2)
 		ctx.monitor.worked(10)
 		
+		api.info(self.model.meta, "airplane.passengerscapacity.success", [])
 		return self.ctx
