5c5a18d3938e56dd409a2797418b207a3e521285
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2010-06-15 13:10:41 +0000

    date pattern on preferences

--- wind-platform/wind-faces/src/br/com/maisha/wind/faces/view/GridViewLabelProvider.java
+++ wind-platform/wind-faces/src/br/com/maisha/wind/faces/view/GridViewLabelProvider.java
@@ -1,10 +1,15 @@
 package br.com.maisha.wind.faces.view;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Map;
 
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
+
+import br.com.maisha.wind.controller.message.PlatformMessageRegistry;
 
 /**
  * 
@@ -13,7 +18,11 @@ import org.eclipse.swt.graphics.Image;
  */
 public class GridViewLabelProvider extends LabelProvider implements ITableLabelProvider {
 
+	/** */
 	private Map<Integer, String> properties;
+
+	/** */
+	private ValueWriter writer = new ValueWriter();
 
 	public GridViewLabelProvider(Map<Integer, String> properties) {
 		this.properties = properties;
@@ -39,7 +48,36 @@ public class GridViewLabelProvider extends LabelProvider implements ITableLabelP
 		String propName = properties.get(columnIndex);
 		Object val = dataMap.get(propName);
 
-		return val != null ? val.toString() : "";
+		return val != null ? writer.write(val) : "";
 	}
 
+	/**
+	 * 
+	 * @author Paulo Freitas (pfreitas1@gmail.com)
+	 * 
+	 */
+	class ValueWriter {
+
+		public String write(Object val) {
+			if (val instanceof Date) {
+				return write((Date) val);
+			}
+
+			return val.toString();
+		}
+
+		/**
+		 * 
+		 * @param date
+		 * @return
+		 */
+		public String write(Date date) {
+			Locale loc = PlatformMessageRegistry.getInstance().getCurrentLocale();
+			String pattern = PlatformMessageRegistry.getInstance().getPreference("datePattern", "dd/MM/yyyy");
+			SimpleDateFormat sdf = new SimpleDateFormat(pattern, loc);
+			return sdf.format(date);
+
+		}
+
+	}
 }
