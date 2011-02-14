e3f113e87cc44fa675644cc052e6a410ece26897
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2011-02-14 19:11:50 +0000

    rule api

--- wind-platform/wind-sample/src/Groovy.groovy
+++ wind-platform/wind-sample/src/Groovy.groovy
@@ -1,36 +1,15 @@
 import br.com.maisha.wind.common.factory.ServiceProvider;
 
-import org.osgi.framework.BundleContext;
-import org.osgi.service.prefs.Preferences;
-import org.osgi.service.prefs.PreferencesService;
-
 class GroovyAction{
 
-	def ctx
-	def api
-	def model
+	def ctx;
+	def model;
 
-	GroovyAction(ctx, api){
-		this.ctx = ctx
-		this.api = api
-		this.model = ctx.instance
-	}
-	
 	Object execute(){
-		String.metaClass.warn={->
-			api.warn(model.meta, delegate, null)
-		}
-	
-		BundleContext bctx = ctx.operation.domainObject.application.bundleContext
-		PreferencesService prefService = ServiceProvider.getInstance().getService(PreferencesService.class, bctx);
-		
-		
-		Preferences generalPref  = prefService.getSystemPreferences().node("general")
-		generalPref.put("currentLocale", "pt_BR")
-		
-		model.code =  prefService.getSystemPreferences().node("general").get("currentLocale", "none")		
+		"airplane.passengerscapacity.weight.required".warnWithParam(["aa", "bb"])
+
+		model.save();
+				
 		return ctx
 	}
-	
-	
 }
