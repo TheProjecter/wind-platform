cde677d5f8c247098d54a416c88efc6caa7f156f
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2011-05-10 19:31:11 +0000

    alteracoes para desregistrar aplicativos da plataforma

--- wind-platform/wind-app-controller/src/br/com/maisha/wind/lifecycle/WindActivator.java
+++ wind-platform/wind-app-controller/src/br/com/maisha/wind/lifecycle/WindActivator.java
@@ -9,9 +9,8 @@ import br.com.maisha.wind.controller.rcp.Activator;
 import br.com.maisha.wind.lifecycle.mgmt.IApplicationManager;
 
 /**
- * This class must be inherited by all bundles who wants to become a wind
- * application. By doing this, a bundle will get all it's Wind objects parsed
- * and registered into the platform.
+ * This class must be inherited by all bundles who wants to become a wind application. By doing this, a bundle will get all it's Wind
+ * objects parsed and registered into the platform.
  * 
  * @author Paulo Freitas (pfreitas1@gmail.com)
  * 
@@ -26,15 +25,13 @@ public class WindActivator implements BundleActivator {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		log.info(context.getBundle().getSymbolicName() + " ["
-				+ context.getBundle().getBundleId() + "] Starting... ");
+		log.info(context.getBundle().getSymbolicName() + " [" + context.getBundle().getBundleId() + "] Starting... ");
 
-		IApplicationManager appManager = ServiceProvider.getInstance()
-				.getService(IApplicationManager.class, Activator.getDefault().getBundle().getBundleContext());
+		IApplicationManager appManager = ServiceProvider.getInstance().getService(IApplicationManager.class,
+				Activator.getDefault().getBundle().getBundleContext());
 		appManager.registerApplication(context);
 
-		log.info(context.getBundle().getSymbolicName() + " ["
-				+ context.getBundle().getBundleId() + "] Started ");
+		log.info(context.getBundle().getSymbolicName() + " [" + context.getBundle().getBundleId() + "] Started ");
 	}
 
 	/**
@@ -42,12 +39,13 @@ public class WindActivator implements BundleActivator {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		log.info(context.getBundle().getSymbolicName() + " ["
-				+ context.getBundle().getBundleId() + "] Stopping... ");
+		log.info(context.getBundle().getSymbolicName() + " [" + context.getBundle().getBundleId() + "] Stopping... ");
 
+		IApplicationManager appManager = ServiceProvider.getInstance().getService(IApplicationManager.class,
+				Activator.getDefault().getBundle().getBundleContext());
+		appManager.unregisterApplication(context);
 
-		log.info(context.getBundle().getSymbolicName() + " ["
-				+ context.getBundle().getBundleId() + "] Stopped... ");
+		log.info(context.getBundle().getSymbolicName() + " [" + context.getBundle().getBundleId() + "] Stopped... ");
 	}
 
 }
--- wind-platform/wind-app-controller/src/br/com/maisha/wind/lifecycle/mgmt/IApplicationManager.java
+++ wind-platform/wind-app-controller/src/br/com/maisha/wind/lifecycle/mgmt/IApplicationManager.java
@@ -19,6 +19,12 @@ public interface IApplicationManager extends Component {
 
 	/**
 	 * 
+	 * @param context
+	 */
+	void unregisterApplication(BundleContext context);
+	
+	/**
+	 * 
 	 * @param appId
 	 * @param objectId
 	 */
