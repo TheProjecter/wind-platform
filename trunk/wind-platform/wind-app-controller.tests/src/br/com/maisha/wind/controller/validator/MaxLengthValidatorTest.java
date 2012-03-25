de17ae09e021f6f3e59a09664f32331fde2ac4cc
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2012-03-25 17:15:26 +0000

    Share project "wind-app-controller.tests" into "https://wind-platform.googlecode.com/svn/trunk/wind-platform"

---
+++ trunk/wind-platform/wind-app-controller.tests/src/br/com/maisha/wind/controller/validator/MaxLengthValidatorTest.java
@@ -0,0 +1,25 @@
+package br.com.maisha.wind.controller.validator;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import br.com.maisha.terra.lang.Property;
+
+public class MaxLengthValidatorTest {
+
+	@Test
+	public void testValidate() {
+		MaxLengthValidator o = new MaxLengthValidator();
+		o.configure(new Property("max_length", "10"));
+		Assert.assertTrue("O valor [12345678901] nao deve passar na validacao de max length(10)", o.validate("12345678901"));
+	}
+
+	@Test
+	public void testValidate2() {
+		MaxLengthValidator o = new MaxLengthValidator();
+		o.configure(new Property("max_length", "10"));
+		Assert.assertTrue(o.validate("1234567"));
+	}
+
+}
