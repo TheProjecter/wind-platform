6373e798ba955870d90751981888ed7232f0586a
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2012-03-27 00:05:44 +0000

    corrigindo teste

--- wind-platform/wind-app-controller.tests/src/br/com/maisha/wind/controller/validator/MinLengthValidatorTest.java
+++ wind-platform/wind-app-controller.tests/src/br/com/maisha/wind/controller/validator/MinLengthValidatorTest.java
@@ -10,14 +10,14 @@ public class MinLengthValidatorTest {
 
 	@Test
 	public void testValidate() {
-		MaxLengthValidator o = new MaxLengthValidator();
+		MinLengthValidator o = new MinLengthValidator();
 		o.configure(new Property("min_length", "3"));
 		Assert.assertFalse("O valor [12] nao deve passar na validacao de min length(3)", o.validate("12"));
 	}
 
 	@Test
 	public void testValidate2() {
-		MaxLengthValidator o = new MaxLengthValidator();
+		MinLengthValidator o = new MinLengthValidator();
 		o.configure(new Property("min_length", "3"));
 		Assert.assertTrue("O valor [123] deve passar na validacao de min length(3)", o.validate("123"));
 	}
