8caeb87288594d54a7eb63d17f3881c32ef90e2b
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2012-06-20 02:22:18 +0000

    select source folder and package

--- trunk/wind-app-tooling/wind-app-tooling/src/br/com/maisha/wind/app/tooling/wizards/NewDmoWizardPage.java
+++ trunk/wind-app-tooling/wind-app-tooling/src/br/com/maisha/wind/app/tooling/wizards/NewDmoWizardPage.java
@@ -1,19 +1,8 @@
 package br.com.maisha.wind.app.tooling.wizards;
 
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.jdt.core.IJavaModel;
-import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.ui.JavaElementComparator;
-import org.eclipse.jdt.ui.JavaElementLabelProvider;
-import org.eclipse.jdt.ui.StandardJavaElementContentProvider;
-import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.jface.viewers.ViewerFilter;
-import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
@@ -24,18 +13,39 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.dialogs.ElementTreeSelectionDialog;
 
+/**
+ * 
+ * @author Paulo Freitas (pfreitas1@gmail.com)
+ * 
+ */
 public class NewDmoWizardPage extends WizardPage {
 
+	/** */
 	private Text sourceFolder;
-	
+
+	/** */
+	private Text packge;
+
+	/** */
+	private IStructuredSelection selection;
+
+	/**
+	 * Construtor configura objeto selecionado no workbench
+	 * 
+	 * @param selecion
+	 */
 	public NewDmoWizardPage(IStructuredSelection selecion) {
 		super("NewDmoWizardPage");
 		setTitle("Domain Object");
 		setDescription("Creates a new Domain Object file");
+		this.selection = selecion;
 	}
 
+	/**
+	 * 
+	 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
+	 */
 	@Override
 	public void createControl(Composite parent) {
 		Composite panel = new Composite(parent, SWT.NONE);
@@ -45,19 +55,19 @@ public class NewDmoWizardPage extends WizardPage {
 		GridData srcTextGD = new GridData();
 		srcTextGD.grabExcessHorizontalSpace = true;
 		srcTextGD.horizontalAlignment = SWT.FILL;
-		
+
 		GridData textGD = new GridData();
 		textGD.grabExcessHorizontalSpace = true;
 		textGD.horizontalAlignment = SWT.FILL;
 		textGD.horizontalSpan = 2;
-		
+
 		GridData sepGD = new GridData();
 		sepGD.horizontalAlignment = SWT.FILL;
 		sepGD.grabExcessHorizontalSpace = true;
 		sepGD.horizontalSpan = 3;
 
-		Label l1 = new Label(panel, SWT.NONE);
-		l1.setText("Source folder:");
+		Label srcFolderLabel = new Label(panel, SWT.NONE);
+		srcFolderLabel.setText("Source folder:");
 		sourceFolder = new Text(panel, SWT.BORDER);
 		sourceFolder.setLayoutData(srcTextGD);
 
@@ -65,16 +75,33 @@ public class NewDmoWizardPage extends WizardPage {
 		fBrowseButton.setText("Browse");
 		fBrowseButton.addSelectionListener(new SelectionListener() {
 			public void widgetDefaultSelected(SelectionEvent e) {
-				IPackageFragmentRoot srcFolder = chooseContainer();
-				String projectName = srcFolder.getJavaProject().getElementName();
-				projectName += "/"+ srcFolder.getElementName();
-				sourceFolder.setText(projectName);
+				widgetSelected(e);
 			}
 
 			public void widgetSelected(SelectionEvent e) {
 				IPackageFragmentRoot srcFolder = chooseContainer();
 				String projectName = srcFolder.getJavaProject().getElementName();
-				projectName += "/"+ srcFolder.getElementName();
+				projectName += "/" + srcFolder.getElementName();
+				sourceFolder.setText(projectName);
+			}
+		});
+
+		Label pkgLabel = new Label(panel, SWT.NONE);
+		pkgLabel.setText("Package:");
+		packge = new Text(panel, SWT.BORDER);
+		packge.setLayoutData(srcTextGD);
+
+		Button fBrowsePkgButton = new Button(panel, SWT.PUSH);
+		fBrowsePkgButton.setText("Browse");
+		fBrowsePkgButton.addSelectionListener(new SelectionListener() {
+			public void widgetDefaultSelected(SelectionEvent e) {
+				widgetSelected(e);
+			}
+
+			public void widgetSelected(SelectionEvent e) {
+				IPackageFragment srcFolder = choosePackage();
+				String projectName = srcFolder.getJavaProject().getElementName();
+				projectName += "/" + srcFolder.getElementName();
 				sourceFolder.setText(projectName);
 			}
 		});
@@ -83,60 +110,33 @@ public class NewDmoWizardPage extends WizardPage {
 		sep.setLayoutData(sepGD);
 
 		Label l4 = new Label(panel, SWT.NONE);
-		l4.setText("Qualified Name:");
+		l4.setText("Name:");
 		Text dmoName = new Text(panel, SWT.BORDER);
 		dmoName.setLayoutData(textGD);
-
-
 
 		setControl(parent);
 	}
 
+	/**
+	 * Abre o dialogo para escolha do container (src folder)
+	 * 
+	 * @return Src Folder Selecionado
+	 */
 	protected IPackageFragmentRoot chooseContainer() {
-
-		final Class<?>[] acceptedClasses = new Class<?>[] { IJavaModel.class, IPackageFragmentRoot.class, IJavaProject.class };
-		ViewerFilter filter = new ViewerFilter() {
-			public boolean select(Viewer viewer, Object parentElement, Object element) {
-				if (element instanceof IPackageFragmentRoot) {
-					try {
-						return (((IPackageFragmentRoot) element).getKind() == IPackageFragmentRoot.K_SOURCE);
-					} catch (JavaModelException e) {
-						return false;
-					}
-				}
-				
-				for(Class<?> acceptedClass: acceptedClasses){
-					if(acceptedClass.isInstance(element)){
-						return true;
-					}
-				}
-				return false;
-			}
-		};
-
-		StandardJavaElementContentProvider provider = new StandardJavaElementContentProvider();
-		ILabelProvider labelProvider = new JavaElementLabelProvider(JavaElementLabelProvider.SHOW_DEFAULT);
-		ElementTreeSelectionDialog dialog = new ElementTreeSelectionDialog(getShell(), labelProvider, provider);
-		dialog.setComparator(new JavaElementComparator());
-		dialog.setTitle("Source Folder Selection");
-		dialog.setMessage("Select the source folder for your Domain Object");
-		dialog.setInput(JavaCore.create(ResourcesPlugin.getWorkspace().getRoot()));
-		dialog.addFilter(filter);
-		dialog.setHelpAvailable(false);
-
-		if (dialog.open() == Window.OK) {
-			Object element = dialog.getFirstResult();
-			if (element instanceof IJavaProject) {
-				IJavaProject jproject = (IJavaProject) element;
-				return jproject.getPackageFragmentRoot(jproject.getProject());
-			} else if (element instanceof IPackageFragmentRoot) {
-				return (IPackageFragmentRoot) element;
-			}
-			return null;
-		}
-		return null;
+		PackageFragmentSelectionDialog<IPackageFragmentRoot> dialog = new PackageFragmentSelectionDialog<IPackageFragmentRoot>(
+				getShell(), IPackageFragmentRoot.class);
+		return dialog.open();
 	}
 
-	
+	/**
+	 * Abre o dialogo para escolha do container (src folder)
+	 * 
+	 * @return Src Folder Selecionado
+	 */
+	protected IPackageFragment choosePackage() {
+		PackageFragmentSelectionDialog<IPackageFragment> dialog = new PackageFragmentSelectionDialog<IPackageFragment>(
+				getShell(), IPackageFragment.class);
+		return dialog.open();
+	}
 	
 }
