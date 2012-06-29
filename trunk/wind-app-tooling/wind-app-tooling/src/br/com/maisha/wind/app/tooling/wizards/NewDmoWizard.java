701ee088422ff6fc47b4c7560243fb22efd4aee7
pfreitas1 <pfreitas1@7bc07f76-e8d3-11dd-b2d9-215335d0b316>
2012-06-29 15:42:09 +0000

    new dmo wizard

--- trunk/wind-app-tooling/wind-app-tooling/src/br/com/maisha/wind/app/tooling/wizards/NewDmoWizard.java
+++ trunk/wind-app-tooling/wind-app-tooling/src/br/com/maisha/wind/app/tooling/wizards/NewDmoWizard.java
@@ -1,36 +1,203 @@
 package br.com.maisha.wind.app.tooling.wizards;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.templates.DocumentTemplateContext;
+import org.eclipse.jface.text.templates.Template;
+import org.eclipse.jface.text.templates.TemplateBuffer;
+import org.eclipse.jface.text.templates.TemplateContext;
+import org.eclipse.jface.text.templates.TemplateException;
+import org.eclipse.jface.text.templates.TemplateTranslator;
+import org.eclipse.jface.text.templates.persistence.TemplatePersistenceData;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
 
+import br.com.maisha.wind.app.tooling.preferences.template.WindTemplatesContextType;
+import br.com.maisha.wind.app.tooling.preferences.template.WindTemplatesVariableResolver;
+
+import com.maisha.wind.editor.preferences.template.TerraCustomTemplateManager;
+
+/**
+ * Wizard responsavel pela criacao de novos arquivos de definicao de Domain
+ * Object (*.dmo)
+ * 
+ * @author Paulo Freitas (pfreitas1@gmail.com)
+ */
 public class NewDmoWizard extends Wizard implements INewWizard {
 
+	/** Selecao no momento da abertura do wizard. */
 	private IStructuredSelection selection;
+
+	/** Primeira e unica pagina (por enquanto, talvez). */
 	private NewDmoWizardPage newDmoWizardPage;
-	private IWorkbench workbench;
 
+	/**
+	 * Construtor default.
+	 */
 	public NewDmoWizard() {
-
 		setWindowTitle("New Config File");
 
 	}
 
-	@Override
+	/**
+	 * 
+	 * @see org.eclipse.jface.wizard.Wizard#addPages()
+	 */
 	public void addPages() {
 		newDmoWizardPage = new NewDmoWizardPage(selection);
 		addPage(newDmoWizardPage);
 	}
 
-	@Override
+	/**
+	 * 
+	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
+	 */
 	public boolean performFinish() {
+		if (newDmoWizardPage.isPageComplete()) {
+			final String dmoName = newDmoWizardPage.getDomainObjectName();
+			final String pkg = newDmoWizardPage.getPackageName();
+			final String container = newDmoWizardPage.getSourceFolderName() + "/" + pkg.replaceAll("\\.", "/");
 
-		return true;
+			IRunnableWithProgress op = new IRunnableWithProgress() {
+				public void run(IProgressMonitor monitor) throws InvocationTargetException {
+					try {
+						doFinish(container, pkg, dmoName, monitor);
+					} catch (CoreException e) {
+						throw new InvocationTargetException(e);
+					} finally {
+						monitor.done();
+					}
+				}
+			};
+			try {
+				getContainer().run(true, false, op);
+			} catch (InterruptedException e) {
+				return false;
+			} catch (InvocationTargetException e) {
+				Throwable realException = e.getTargetException();
+				MessageDialog.openError(getShell(), "Error", realException.getMessage());
+				return false;
+			}
+
+			return true;
+		}
+		return false;
 	}
 
+	/**
+	 * 
+	 * @param containerName
+	 * @param fileName
+	 * @param monitor
+	 * @throws CoreException
+	 */
+	private void doFinish(String containerName, String pkg, String fileName, IProgressMonitor monitor)
+			throws CoreException {
+		monitor.beginTask("Creating " + fileName, 2);
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IResource resource = root.findMember(new Path(containerName));
+		if (!resource.exists() || !(resource instanceof IContainer)) {
+			throwCoreException("Container \"" + containerName + "\" does not exist.");
+		}
+		IContainer container = (IContainer) resource;
+		final IFile file = container.getFile(new Path(fileName + ".dmo"));
+		try {
+			InputStream stream = openContentStream(pkg, fileName);
+			if (file.exists()) {
+				file.setContents(stream, true, true, monitor);
+			} else {
+				file.create(stream, true, monitor);
+			}
+			stream.close();
+		} catch (IOException e) {
+		}
+		monitor.worked(1);
+		monitor.setTaskName("Opening file for editing...");
+		getShell().getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				try {
+					IDE.openEditor(page, file, true);
+				} catch (PartInitException e) {
+				}
+			}
+		});
+		monitor.worked(1);
+	}
+
+	/**
+	 * 
+	 * @return
+	 * @throws CoreException
+	 */
+	private InputStream openContentStream(String pkgName, String dmoName) throws CoreException {
+		TemplatePersistenceData dmoClassTD = TerraCustomTemplateManager.getInstance().getTemplateStore()
+				.getTemplateData("wind.editor.ui.templates.class");
+		Template template = dmoClassTD.getTemplate();
+
+		try {
+			TemplateContext context = new TemplateContext(new WindTemplatesContextType()) {
+				public TemplateBuffer evaluate(Template template) throws BadLocationException, TemplateException {
+					TemplateTranslator translator = new TemplateTranslator();
+					TemplateBuffer buffer = translator.translate(template);
+					getContextType().resolve(buffer, this);
+					return buffer;
+				}
+
+				public boolean canEvaluate(Template template) {
+					return true;
+				}
+			};
+
+			context.setVariable(WindTemplatesVariableResolver.PKG_VARIABLE, pkgName);
+			context.setVariable(WindTemplatesVariableResolver.DMO_NAME_VARIABLE, dmoName);
+
+			return new ByteArrayInputStream(context.evaluate(template).getString().getBytes());
+		} catch (Exception e) {
+			throwCoreException(e.getMessage());
+		}
+
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param message
+	 * @throws CoreException
+	 */
+	private void throwCoreException(String message) throws CoreException {
+		IStatus status = new Status(IStatus.ERROR, "sampleeditor", IStatus.OK, message, null);
+		throw new CoreException(status);
+	}
+
+	/**
+	 * 
+	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench,
+	 *      org.eclipse.jface.viewers.IStructuredSelection)
+	 */
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		this.workbench = workbench;
 		this.selection = selection;
 	}
 
--- trunk/wind-app-tooling/wind-app-tooling/src/br/com/maisha/wind/app/tooling/wizards/NewDmoWizardPage.java
+++ trunk/wind-app-tooling/wind-app-tooling/src/br/com/maisha/wind/app/tooling/wizards/NewDmoWizardPage.java
@@ -1,10 +1,20 @@
 package br.com.maisha.wind.app.tooling.wizards;
 
+import static br.com.maisha.wind.app.tooling.util.Utilities.isNotBlank;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaModel;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
@@ -14,20 +24,29 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
+import br.com.maisha.wind.app.tooling.dialogs.PackageFragmentSelectionDialog;
+
 /**
+ * Wizard Page responsavel pela criacao de definicoes de Domain Object (arquivos
+ * *.dmo)
  * 
  * @author Paulo Freitas (pfreitas1@gmail.com)
- * 
  */
 public class NewDmoWizardPage extends WizardPage {
 
-	/** */
-	private Text sourceFolder;
+	/**
+	 * Campo de texto que recebe o source folder onde o arquivo *.dmo sera
+	 * criado.
+	 */
+	private Text sourceFolderText;
 
-	/** */
-	private Text packge;
+	/** Campo de texto que recebe o pacote (i.e pkg java) para o Domain Object */
+	private Text packageText;
 
-	/** */
+	/** Campo de texto que recebe o nome do Domain Object. */
+	private Text dmoNameText;
+
+	/** Selecao do Workbench no momento da abertura do Wizard. */
 	private IStructuredSelection selection;
 
 	/**
@@ -40,6 +59,7 @@ public class NewDmoWizardPage extends WizardPage {
 		setTitle("Domain Object");
 		setDescription("Creates a new Domain Object file");
 		this.selection = selecion;
+
 	}
 
 	/**
@@ -68,8 +88,13 @@ public class NewDmoWizardPage extends WizardPage {
 
 		Label srcFolderLabel = new Label(panel, SWT.NONE);
 		srcFolderLabel.setText("Source folder:");
-		sourceFolder = new Text(panel, SWT.BORDER);
-		sourceFolder.setLayoutData(srcTextGD);
+		sourceFolderText = new Text(panel, SWT.BORDER);
+		sourceFolderText.setLayoutData(srcTextGD);
+		sourceFolderText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				setPageComplete(isPageComplete());
+			}
+		});
 
 		Button fBrowseButton = new Button(panel, SWT.PUSH);
 		fBrowseButton.setText("Browse");
@@ -80,16 +105,19 @@ public class NewDmoWizardPage extends WizardPage {
 
 			public void widgetSelected(SelectionEvent e) {
 				IPackageFragmentRoot srcFolder = chooseContainer();
-				String projectName = srcFolder.getJavaProject().getElementName();
-				projectName += "/" + srcFolder.getElementName();
-				sourceFolder.setText(projectName);
+				setSourceFolderText(srcFolder);
 			}
 		});
 
 		Label pkgLabel = new Label(panel, SWT.NONE);
 		pkgLabel.setText("Package:");
-		packge = new Text(panel, SWT.BORDER);
-		packge.setLayoutData(srcTextGD);
+		packageText = new Text(panel, SWT.BORDER);
+		packageText.setLayoutData(srcTextGD);
+		packageText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				setPageComplete(isPageComplete());
+			}
+		});
 
 		Button fBrowsePkgButton = new Button(panel, SWT.PUSH);
 		fBrowsePkgButton.setText("Browse");
@@ -99,10 +127,8 @@ public class NewDmoWizardPage extends WizardPage {
 			}
 
 			public void widgetSelected(SelectionEvent e) {
-				IPackageFragment srcFolder = choosePackage();
-				String projectName = srcFolder.getJavaProject().getElementName();
-				projectName += "/" + srcFolder.getElementName();
-				sourceFolder.setText(projectName);
+				IPackageFragment pkg = choosePackage();
+				setPackageText(pkg);
 			}
 		});
 
@@ -111,10 +137,80 @@ public class NewDmoWizardPage extends WizardPage {
 
 		Label l4 = new Label(panel, SWT.NONE);
 		l4.setText("Name:");
-		Text dmoName = new Text(panel, SWT.BORDER);
-		dmoName.setLayoutData(textGD);
+		dmoNameText = new Text(panel, SWT.BORDER);
+		dmoNameText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				setPageComplete(isPageComplete());
+			}
+		});
+		dmoNameText.setLayoutData(textGD);
 
+		processSelection();
 		setControl(parent);
+	}
+
+	/**
+	 * Processa a selecao corrente no Workbench preenchendo os campos de texto
+	 * Source Folder e Package de acordo com o elemento selecionado.
+	 */
+	private void processSelection() {
+		try {
+			if (selection.isEmpty() || selection.getFirstElement() == null) {
+				return;
+			}
+
+			if (selection.getFirstElement() instanceof IFile) {
+				IFile file = (IFile) selection.getFirstElement();
+				IJavaElement javaPkg = JavaCore.create(file.getParent());
+				if (javaPkg != null && javaPkg instanceof IPackageFragment) {
+					setPackageText((IPackageFragment) javaPkg);
+					setSourceFolderText((IPackageFragmentRoot) javaPkg.getParent());
+				}
+			}
+
+			if (selection.getFirstElement() instanceof IFolder) {
+				IFolder first = (IFolder) selection.getFirstElement();
+				IJavaElement javaModel = JavaCore.create(first);
+
+				if (javaModel != null && javaModel instanceof IPackageFragmentRoot) {
+					IPackageFragmentRoot fragRoot = (IPackageFragmentRoot) javaModel;
+					if (fragRoot.getKind() == IPackageFragmentRoot.K_SOURCE) {
+						setSourceFolderText(fragRoot);
+					}
+				} else if (javaModel != null && javaModel instanceof IPackageFragment) {
+					setPackageText((IPackageFragment) javaModel);
+					setSourceFolderText((IPackageFragmentRoot) javaModel.getParent());
+
+				}
+			}
+		} catch (Exception e) {
+			// silently ignore
+		}
+
+	}
+
+	/**
+	 * Configura o valor do campo de texto Source Folder com base no
+	 * {@link IPackageFragmentRoot} dado
+	 * 
+	 * @param fragRoot
+	 *            Representacao do Src Folder
+	 */
+	private void setSourceFolderText(IPackageFragmentRoot fragRoot) {
+		String projectName = fragRoot.getJavaProject().getElementName();
+		projectName += "/" + fragRoot.getElementName();
+		sourceFolderText.setText(projectName);
+	}
+
+	/**
+	 * Configura o valor do campo de texto Package com base no
+	 * {@link IPackageFragment} dado
+	 * 
+	 * @param pkg
+	 *            Representacao do Package
+	 */
+	private void setPackageText(IPackageFragment pkg) {
+		packageText.setText(pkg.getElementName());
 	}
 
 	/**
@@ -125,18 +221,60 @@ public class NewDmoWizardPage extends WizardPage {
 	protected IPackageFragmentRoot chooseContainer() {
 		PackageFragmentSelectionDialog<IPackageFragmentRoot> dialog = new PackageFragmentSelectionDialog<IPackageFragmentRoot>(
 				getShell(), IPackageFragmentRoot.class);
+		dialog.setAcceptedClasses(new Class<?>[] { IJavaModel.class, IPackageFragmentRoot.class, IJavaProject.class });
+		dialog.setTitle("Source Folder Selection");
+		dialog.setMessage("Choose a source folder:");
 		return dialog.open();
 	}
 
 	/**
-	 * Abre o dialogo para escolha do container (src folder)
+	 * Abre o dialogo para escolha do pacote (package)
 	 * 
-	 * @return Src Folder Selecionado
+	 * @return Pkg Folder Selecionado
 	 */
 	protected IPackageFragment choosePackage() {
 		PackageFragmentSelectionDialog<IPackageFragment> dialog = new PackageFragmentSelectionDialog<IPackageFragment>(
 				getShell(), IPackageFragment.class);
+		dialog.setAcceptedClasses(new Class<?>[] { IJavaModel.class, IPackageFragmentRoot.class, IJavaProject.class,
+				IPackageFragment.class });
+		dialog.setTitle("Package Selection");
+		dialog.setMessage("Choose a package:");
 		return dialog.open();
 	}
-	
+
+	/**
+	 * 
+	 * @see org.eclipse.jface.wizard.WizardPage#isPageComplete()
+	 */
+	public boolean isPageComplete() {
+		return isNotBlank(getDomainObjectName()) && isNotBlank(getSourceFolderName()) && isNotBlank(getPackageName());
+	}
+
+	/**
+	 * Obtem o nome do Domain Object informado pelo usuario.
+	 * 
+	 * @return Nome do Domain Object
+	 */
+	public String getDomainObjectName() {
+		return dmoNameText.getText();
+	}
+
+	/**
+	 * Obtem o nome do Source Folder onde o Domain Object deve ser criado.
+	 * 
+	 * @return Nome do source folder.
+	 */
+	public String getSourceFolderName() {
+		return sourceFolderText.getText();
+	}
+
+	/**
+	 * Obtem o nome do pacote utilizado pelo Domain Object.
+	 * 
+	 * @return Pacote do Domain Object.
+	 */
+	public String getPackageName() {
+		return packageText.getText();
+	}
+
 }
